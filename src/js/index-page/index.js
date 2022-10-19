6ce685dcd6d2057b4afaae53f525389b8929d378
InnaShtondenko <inna.mycase@gmail.com>
2022-10-19 19:37:25 +0300

    Create index.js

---
+++ src/js/index-page/index.js
@@ -0,0 +1,38 @@
+import { openModalFooter } from '../common/modal-footer';
+import { openModal } from '../common/modal';
+import { searchMovies } from './search';
+import goTopBtn from '../common/goTopBtn';
+import { renderPopularMovies } from './renderPopularMovies';
+import btnWhiteBlack from '../common/btn-white-black';
+import { controlColor, controlColorFilter } from '../common/controlColor';
+import { filterRenderGenre, filterRenderYear } from './filterRender';
+import { refs } from '../common/varriables';
+import '../common/headers-fixed';
+import {langCurrent, setIndexLang} from '../lang/changeLang'
+import { filterRenderGenre, filterRenderYear } from './filterRender';
+import * as extendedSearch from "./extendedSearch";
+
+filterRenderGenre();
+filterRenderYear();
+
+
+refs.mainContainer.addEventListener('click', openModal);
+refs.openModalBtn.addEventListener('click', openModalFooter);
+refs.form.addEventListener('submit', searchMovies);
+refs.enLangBTN.addEventListener('click', ()=>changeLang('en'));
+refs.uaLangBTN.addEventListener('click', ()=>changeLang('ua'));
+
+function changeLang (lang){
+  setIndexLang(lang);
+  filterRenderGenre();
+  filterRenderYear();
+  renderPopularMovies();
+}
+
+const currentLang = langCurrent();
+renderPopularMovies();
+setIndexLang(currentLang);
+goTopBtn();
+btnWhiteBlack();
+controlColor();
+controlColorFilter();
